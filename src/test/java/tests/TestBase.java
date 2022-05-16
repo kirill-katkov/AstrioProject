4324b94f1e22de16260663033c78adb3f9c012de
Kirill Katkov <k.katkov@dodopizza.com>
2022-05-16 07:35:38 +0300

    Update v2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,7 +3,9 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 
+import config.CredentialsConfig;
 import config.Project;
+import helpers.AllureAttachments;
 import helpers.Attach;
 import helpers.DriverSettings;
 import helpers.DriverUtils;
@@ -22,23 +24,27 @@ public class TestBase {
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        DriverSettings.configure();
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "100");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        //Configuration.baseUrl = System.getProperty("baseUrl", "https://hh.ru");
+        Configuration.remote = "https://"+ config.login() + ":" + config.password() + "@" +
+                System.getProperty("remote","selenoid.autotests.cloud/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
     void addAttachments() {
-        String sessionId = DriverUtils.getSessionId();
-
-        Attach.addScreenshotAs("Last screenshot");
-        Attach.addPageSource();
-        Attach.addBrowserConsoleLogs();
-
-        // Selenide.closeWebDriver();
-
-        if (Project.isVideoOn()) {
-            Attach.addVideo(sessionId);
-        }
-        Selenide.closeWebDriver();
+        AllureAttachments.screenshotAs("Screenshot");
+        AllureAttachments.pageSource();
+        AllureAttachments.browserConsoleLogs();
+        AllureAttachments.addVideo();
     }
 
 
